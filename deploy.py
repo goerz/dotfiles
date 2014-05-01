708ff729233c935ec8046ec8fe0c99ddea514122
Michael Goerz <goerz@physik.uni-kassel.de>
2014-05-01 21:06:14 +0200

    Used dotfiles.py from origin/master, not master

--- deploy.py
+++ deploy.py
@@ -10,7 +10,7 @@ from functools import partial
 
 # bootstrap code to get the dotfiles.py module from the master branch
 os.chdir(os.path.split(os.path.realpath(__file__))[0])
-os.system(r'git cat-file -p $(git ls-tree master "dotfiles.py" | cut -d " " -f 3 | cut -f 1) > dotfiles.py')
+os.system(r'git cat-file -p $(git ls-tree origin/master "dotfiles.py" | cut -d " " -f 3 | cut -f 1) > dotfiles.py')
 
 from dotfiles import make_link, git_update, run_duti, main, DOT
 
