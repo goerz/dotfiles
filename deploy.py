21cf2cda67c3addd37532111f8a9192f46c1354d
Michael Goerz <goerz@physik.uni-kassel.de>
2014-05-02 02:54:03 +0200

    Download scripts

--- deploy.py
+++ deploy.py
@@ -10,7 +10,20 @@ import os
 os.chdir(os.path.split(os.path.realpath(__file__))[0])
 os.system(r'git cat-file -p $(git ls-tree origin/master "dotfiles.py" | cut -d " " -f 3 | cut -f 1) > dotfiles.py')
 
-from dotfiles import dot_link, make_link, deploy_vim, main
+from dotfiles import dot_link, deploy_vim, get, main
+
+def get_scripts(options):
+    """ Download various scripts into ~/bin """
+    get('https://raw.githubusercontent.com/goerz/git-graph/master/git-graph.py',
+         'bin/git-graph', options, make_exec=True)
+    get('http://michaelgoerz.net/blog/2009/10/three-little-git-scripts/git-cat',
+        'bin/git-cat', options, make_exec=True)
+    get('http://michaelgoerz.net/blog/2009/10/three-little-git-scripts/git-vimdiff',
+        'bin/git-vimdiff', options, make_exec=True)
+    get('https://raw.githubusercontent.com/goerz/git-repos/master/git-repos',
+        'bin/git-repos', options, make_exec=True)
+    get('http://beyondgrep.com/ack-2.12-single-file',
+        'bin/ack', options, make_exec=True)
 
 def deploy(options):
     """ Routine to be called by dotfiles.main. It will be supplied the parsed
@@ -18,5 +31,6 @@ def deploy(options):
     """
     dot_link(options)
     deploy_vim('git@github.com:goerz/vimrc.git', options)
+    get_scripts(options)
 
 main(deploy)
