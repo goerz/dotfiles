bf5277733e68bab11e01f092653ba2647f909756
Michael Goerz <goerz@physik.uni-kassel.de>
2014-11-16 00:42:56 +0100

    Updated tests

--- test_dotfiles.py
+++ test_dotfiles.py
@@ -4,6 +4,17 @@ import os
 import shutil
 import dotfiles
 import nose
+import __builtin__
+from os.path import join, isfile, isdir, realpath
+from os import readlink
+"""
+Run tests of dotfiles.py
+
+Options:
+
+--nocapture  print the STDOUT from all tests
+-v           give more information about tests
+"""
 
 class DummyOptions(object):
     def __init__(self, quiet=False, overwrite=False, uninstall=False):
@@ -14,7 +25,7 @@ class DummyOptions(object):
 
 def clean_home():
     try:
-        shutil.rmtree(os.path.join("test", "HOME"))
+        shutil.rmtree(join("test", "HOME"))
     except OSError:
         pass
 
@@ -22,31 +33,44 @@ def clean_home():
 @nose.with_setup(clean_home, clean_home)
 def test_make_link():
 
+    dotfiles.HOME = join('test', 'HOME')
+    dotfiles.DOTFILES = join('test', 'DOTFILES')
+
     # create simple file
     dotfiles.make_link('.bashrc', '.bashrc', DummyOptions())
-    src_file = os.path.join('test', 'DOTFILES', '.bashrc')
-    target_file = os.path.join('test', 'HOME', '.bashrc')
-    assert os.path.isfile(target_file)
+    src_file = join(dotfiles.DOTFILES, '.bashrc')
+    target_file = join(dotfiles.HOME, '.bashrc')
+    assert isfile(target_file)
 
     # If the file exists and is linked correctly, making another link will do
     # nothing
     dotfiles.make_link('.bashrc', '.bashrc', DummyOptions())
-    assert os.path.isfile(target_file)
+    assert isfile(target_file)
 
     # if the file exists as a proper file, an error should be raised
     os.unlink(target_file)
     with open(target_file, 'w') as out_fh:
         out_fh.write("# .bashrc")
     try:
-        dotfiles.make_link('.bashrc', '.bashrc', DummyOptions())
+        dotfiles.make_link('.bashrc', '.bashrc', DummyOptions(quiet=True))
         raise AssertionError("linking over existing proper file should fail")
     except OSError:
         pass
 
-    # unless we give the overwrite option
+    # we may also confirm the overwrite interactively
+    old_raw_input = __builtin__.raw_input
+    __builtin__.raw_input = lambda query: 'yes'
+    dotfiles.make_link('.bashrc', '.bashrc', DummyOptions(quiet=False))
+    __builtin__.raw_input = old_raw_input
+    assert isfile(target_file)
+
+    # or give the overwrite option
+    os.unlink(target_file)
+    with open(target_file, 'w') as out_fh:
+        out_fh.write("# .bashrc")
     dotfiles.make_link('.bashrc', '.bashrc', DummyOptions(overwrite=True))
-    assert os.path.isfile(target_file)
-    assert (os.path.realpath(target_file) == os.path.realpath(src_file))
+    assert isfile(target_file)
+    assert (realpath(target_file) == realpath(src_file))
 
     # What should never work, however, is to overwrite a folder
     os.unlink(target_file)
@@ -60,27 +84,31 @@ def test_make_link():
 
     # The uninstall option should remove the file
     dotfiles.make_link('.bashrc', '.bashrc', DummyOptions())
-    assert os.path.isfile(target_file)
+    assert isfile(target_file)
     dotfiles.make_link('.bashrc', '.bashrc', DummyOptions(uninstall=True))
-    assert not os.path.isfile(target_file)
+    assert not isfile(target_file)
 
     # but only if the file is actually linked correctly
+    dotfiles.mkdir(dotfiles.HOME)
     with open(target_file, 'w') as out_fh:
         out_fh.write("# .bashrc")
-    assert os.path.isfile(target_file)
+    assert isfile(target_file)
     dotfiles.make_link('.bashrc', '.bashrc', DummyOptions(uninstall=True))
-    assert os.path.isfile(target_file)
+    assert isfile(target_file)
 
 
 @nose.with_setup(clean_home, clean_home)
 def test_get():
+    return # DEBUG
+
+    dotfiles.HOME = join('test', 'HOME')
 
     # Standard download
     url = 'https://raw.githubusercontent.com/goerz/dotfiles/master/'\
           'README.markdown'
-    target_file = os.path.join('test', 'HOME', 'README')
+    target_file = join('test', 'HOME', 'README')
     dotfiles.get(url, 'README', DummyOptions(), make_exec=False)
-    assert os.path.isfile(target_file)
+    assert isfile(target_file)
 
     # Should fail if file already exists
     try:
@@ -91,28 +119,76 @@ def test_get():
 
     # Unless overwrite is True
     dotfiles.get(url, 'README', DummyOptions(overwrite=True))
-    assert os.path.isfile(target_file) and not os.access(target_file, os.X_OK)
+    assert isfile(target_file) and not os.access(target_file, os.X_OK)
 
     # If make_exec is True, the resulting file should be executable
     dotfiles.get(url, 'README', DummyOptions(overwrite=True), make_exec=True)
-    assert os.path.isfile(target_file) and os.access(target_file, os.X_OK)
+    assert isfile(target_file) and os.access(target_file, os.X_OK)
 
 
-#@nose.with_setup(clean_home, clean_home)
+@nose.with_setup(clean_home, clean_home)
 def test_deploy_vim():
-    dotfiles.mkdir(os.path.join('test', 'HOME'))
+
+    dotfiles.HOME = join('test', 'HOME')
+    dotfiles.DOTFILES = join('test', 'DOTFILES')
+
+    return # DEBUG
+    dotfiles.mkdir(dotfiles.HOME)
     dotfiles.deploy_vim('https://github.com/goerz/vimrc.git', DummyOptions())
-    assert os.path.isfile(os.path.join('test', 'HOME', '.vimrc'))
-    assert os.path.isdir(os.path.join('test', 'HOME', '.vim'))
-    assert os.path.isfile(os.path.join('test', 'HOME', '.vim', 'vimrc'))
+    assert isfile(join(dotfiles.HOME, '.vimrc'))
+    assert isdir(join(dotfiles.HOME, '.vim'))
+    assert isfile(join(dotfiles.HOME, '.vim', 'vimrc'))
     # A second call should update the repository
     dotfiles.deploy_vim('https://github.com/goerz/vimrc.git', DummyOptions())
 
 
+@nose.with_setup(clean_home, clean_home)
+def test_make_links():
+
+    dotfiles.HOME = join('test', 'HOME')
+    shutil.copytree(join('test', 'DOTFILES'),
+                    join(dotfiles.HOME, '.dotfiles', 'HOME'))
+    dotfiles.DOTFILES = join(dotfiles.HOME, '.dotfiles')
+
+    dotfiles.make_links('HOME', DummyOptions())
+
+    def check_link(link_file, dest):
+        assert readlink(os.sep.join(link_file.split("/"))) \
+                == os.sep.join(dest.split("/"))
+
+    check_link('test/HOME/bin/ack', '../.dotfiles/HOME/bin/ack')
+    assert os.access(join(dotfiles.HOME, 'bin', 'ack'), os.X_OK)
+    check_link('test/HOME/.bashrc', '.dotfiles/HOME/.bashrc')
+    check_link('test/HOME/.grace/gracerc.user',
+               '../.dotfiles/HOME/.grace/gracerc.user')
+    check_link('test/HOME/.grace/templates/Default.agr',
+               '../../.dotfiles/HOME/.grace/templates/Default.agr')
+    check_link('test/HOME/.config/Terminal/terminalrc',
+               '../../.dotfiles/HOME/.config/Terminal/terminalrc')
+
+    # Running make_links again should not change anything
+    dotfiles.make_links('HOME', DummyOptions())
+
+    check_link('test/HOME/.bashrc', '.dotfiles/HOME/.bashrc')
+    check_link('test/HOME/.grace/gracerc.user',
+               '../.dotfiles/HOME/.grace/gracerc.user')
+    check_link('test/HOME/.grace/templates/Default.agr',
+               '../../.dotfiles/HOME/.grace/templates/Default.agr')
+    check_link('test/HOME/.config/Terminal/terminalrc',
+               '../../.dotfiles/HOME/.config/Terminal/terminalrc')
+
+    # uninstall should remove all linked files and empty folders
+    dotfiles.make_links('HOME', DummyOptions(uninstall=True))
+    assert isdir(dotfiles.DOTFILES)
+    assert not isfile(join(dotfiles.HOME, '.bashrc'))
+    assert not isdir(join(dotfiles.HOME, '.grace'))
+
+
+
 if __name__ == "__main__":
-    dotfiles.HOME = os.path.join('test', 'HOME')
-    dotfiles.DOTFILES = os.path.join('test', 'DOTFILES')
-    if os.path.isdir(dotfiles.HOME):
+    dotfiles.HOME = join('test', 'HOME')
+    dotfiles.DOTFILES = join('test', 'DOTFILES')
+    if isdir(dotfiles.HOME):
         shutil.rmtree(dotfiles.HOME)
     dotfiles.mkdir(dotfiles.HOME)
     nose.runmodule()
