c12b3a29f1d26bb718aa90ad2aa2a4081438433e
Michael Goerz <goerz@physik.uni-kassel.de>
2014-11-16 01:56:54 +0100

    Remove *.pyc files during bootstrap

--- deploy.py
+++ deploy.py
@@ -8,6 +8,7 @@ import os
 
 # bootstrap code to get the dotfiles.py module from the master branch
 os.chdir(os.path.split(os.path.realpath(__file__))[0])
+os.system(r'rm -f *.pyc')
 os.system(r'git cat-file -p $(git ls-tree origin/master "dotfiles.py" | cut -d " " -f 3 | cut -f 1) > dotfiles.py')
 
 import dotfiles
