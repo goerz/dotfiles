0b0d97e46edff04ee39b7deec808402c3fb82b3b
Michael Goerz <goerz@physik.uni-kassel.de>
2015-09-04 14:07:32 -0700

    Updated deploy script

--- deploy.py
+++ deploy.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-"""Link dotfiles to their appropriate location in HOME"""
+"""Link dotfiles to their appropriate location in $HOME"""
 
 # requires Python >= 2.6
 
@@ -8,29 +8,17 @@ import os
 
 # bootstrap code to get the dotfiles.py module from the master branch
 os.chdir(os.path.split(os.path.realpath(__file__))[0])
+os.system(r'rm -f *.pyc')
 os.system(r'git cat-file -p $(git ls-tree origin/master "dotfiles.py" | cut -d " " -f 3 | cut -f 1) > dotfiles.py')
 
-from dotfiles import dot_link, deploy_vim, get, main
-
-def get_scripts(options):
-    """ Download various scripts into ~/bin """
-    get('https://raw.githubusercontent.com/goerz/git-graph/master/git-graph.py',
-         'bin/git-graph', options, make_exec=True)
-    get('http://michaelgoerz.net/blog/2009/10/three-little-git-scripts/git-cat',
-        'bin/git-cat', options, make_exec=True)
-    get('http://michaelgoerz.net/blog/2009/10/three-little-git-scripts/git-vimdiff',
-        'bin/git-vimdiff', options, make_exec=True)
-    get('https://raw.githubusercontent.com/goerz/git-repos/master/git-repos',
-        'bin/git-repos', options, make_exec=True)
-    get('http://beyondgrep.com/ack-2.12-single-file',
-        'bin/ack', options, make_exec=True)
+import dotfiles
 
 def deploy(options):
     """ Routine to be called by dotfiles.main. It will be supplied the parsed
         command line options
     """
-    dot_link(options)
-    deploy_vim('https://github.com/goerz/vimrc.git', options)
-    get_scripts(options)
+    dotfiles.make_links('HOME', options)
+    dotfiles.deploy_vim('https://github.com/goerz/vimrc.git', options)
+    dotfiles.set_crontab(options.quiet)
 
-main(deploy)
+dotfiles.main(deploy)
