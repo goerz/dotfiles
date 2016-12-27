96df87b6377657d18ef49bbdcc9d9296b7126ee5
Michael Goerz <goerz@stanford.edu>
2016-12-27 13:52:18 -0500

    PEP8

--- dotfiles.py
+++ dotfiles.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 """ Utilitiy functions to deploy dotfiles """
 
 import os
@@ -53,25 +52,25 @@ def is_file_or_link(file):
 
 
 def make_link(src, dst, options):
-    """ Create a symbolic link pointing to src named dst.
+    """Create a symbolic link pointing to src named dst.
 
-        src is a path relative to DOTFILES
-        dst is a path relative to HOME
+    src is a path relative to DOTFILES
+    dst is a path relative to HOME
 
-        If options.overwrite is True and dst already exists, remove the
-        existing file before creating the link.
+    If options.overwrite is True and dst already exists, remove the
+    existing file before creating the link.
 
-        If options.uninstall is True, don't create any symbolic links, but
-        remove them if they exist. Any empty folder that results from this will
-        also be deleted.
+    If options.uninstall is True, don't create any symbolic links, but
+    remove them if they exist. Any empty folder that results from this will
+    also be deleted.
 
-        If the dst already exists and is a symbolic link to src, the routine
-        will exit silently.
+    If the dst already exists and is a symbolic link to src, the routine
+    will exit silently.
 
-        For every newly created link, a message will be printed to screen,
-        unless options.quiet is given als False
+    For every newly created link, a message will be printed to screen,
+    unless options.quiet is given als False
 
-        Raises OSError if an operation cannot be completed
+    Raises OSError if an operation cannot be completed
     """
     global input
     abs_src = os.path.join(DOTFILES, src)
@@ -84,12 +83,12 @@ def make_link(src, dst, options):
             if os.path.isdir(abs_src):
                 shutil.rmtree(abs_dst)
             else:
-                raise OSError("Existing directory %s " % abs_dst
-                            + "would be overwritten by file %s" % abs_src)
-    link_file   = abs_dst
+                raise OSError("Existing directory %s " % abs_dst +
+                              "would be overwritten by file %s" % abs_src)
+    link_file = abs_dst
     link_target = os.path.relpath(abs_src, dst_path)
     if options.uninstall:
-        if (os.path.realpath(abs_dst) == os.path.realpath(abs_src)):
+        if os.path.realpath(abs_dst) == os.path.realpath(abs_src):
             if not options.quiet:
                 print("removing %s" % abs_dst)
             os.unlink(abs_dst)
@@ -100,9 +99,9 @@ def make_link(src, dst, options):
                     os.rmdir(folder)
                     folder = os.path.split(folder)[0]
             except OSError:
-                pass # folder is not empty
+                pass  # folder is not empty
     else:
-        if (os.path.realpath(abs_dst) != os.path.realpath(abs_src)):
+        if os.path.realpath(abs_dst) != os.path.realpath(abs_src):
             if not options.quiet:
                 print("%s -> %s" % (link_file, abs_src))
             if is_file_or_link(dst_path) and not options.quiet:
@@ -155,7 +154,7 @@ def make_links(folder, options, recursive=True, target='.', log_fh=None):
             os.rename(log_filename,
                       change_extension(log_filename, 'old_links'))
         log_fh = open(log_filename, 'w')
-    for file in files: # file is relative to CWD
+    for file in files:  # file is relative to CWD
         src = os.path.relpath(file, DOTFILES)
         dst = os.path.relpath(file, os.path.join(DOTFILES, folder))
         if target != '.':
@@ -163,7 +162,7 @@ def make_links(folder, options, recursive=True, target='.', log_fh=None):
         if os.path.isfile(file):
             make_link(src, dst, options)
             if not options.uninstall:
-                log_fh.write("%s\n" % os.path.abspath(os.path.join(HOME,dst)))
+                log_fh.write("%s\n" % os.path.abspath(os.path.join(HOME, dst)))
         elif os.path.isdir(file):
             if recursive:
                 make_links(src, options, recursive, dst, log_fh)
@@ -172,11 +171,10 @@ def make_links(folder, options, recursive=True, target='.', log_fh=None):
                                  % file)
 
 
-
 def which(program):
-    """ Return the absolute path of the given program, or None if the program
-        is not available -- like Unix which utility)
-    """
+    """Return the absolute path of the given program, or None if the program is
+    not available -- like Unix which utility)"""
+
     def is_exe(fpath):
         """Is fpath an executable?"""
         return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
@@ -196,30 +194,30 @@ def which(program):
 
 
 def deploy_vim(repo, options):
-    """ Deploy vimrc from the given git repository.
+    """Deploy vimrc from the given git repository.
 
-        If ~/.vim does not exist already, the given repo will be cloned to
-        ~/.vim, and a symlink ~/.vimrc -> ~/.vim/init.vim will be created, as
-        well as a symlink $XDG_CONFIG_HOME/nvim -> ~/.vim (where
-        $XDG_CONFIG_HOME defaults to ~/.config)
+    If ~/.vim does not exist already, the given repo will be cloned to
+    ~/.vim, and a symlink ~/.vimrc -> ~/.vim/init.vim will be created, as
+    well as a symlink $XDG_CONFIG_HOME/nvim -> ~/.vim (where
+    $XDG_CONFIG_HOME defaults to ~/.config)
 
-        If ~/.vim does exist and is a git repository, it will be updated to the
-        latest revision.
+    If ~/.vim does exist and is a git repository, it will be updated to the
+    latest revision.
 
-        If options.overwrite is True, and ~/.vim/vimrc exists, any existing
-        file ~/.vimrc will be removed and be replaced with a symlink to
-        ~/.vim/init.vim. Likewise, $XDG_CONFIG_HOME/nvim will be replaced.
+    If options.overwrite is True, and ~/.vim/vimrc exists, any existing
+    file ~/.vimrc will be removed and be replaced with a symlink to
+    ~/.vim/init.vim. Likewise, $XDG_CONFIG_HOME/nvim will be replaced.
 
-        The flag options.uninstall will only affect the file ~/.vimrc and the
-        link $XDG_CONFIG_HOME/nvim; the .vim folder is never touched. This is
-        so that spell check files, buffers, etc. don't get deleted accidentally
+    The flag options.uninstall will only affect the file ~/.vimrc and the
+    link $XDG_CONFIG_HOME/nvim; the .vim folder is never touched. This is
+    so that spell check files, buffers, etc. don't get deleted accidentally
     """
     XDG_CONFIG_HOME = os.environ.get('XDG_CONFIG_HOME',
                                      os.path.join(HOME, ".config"))
-    vimdir       = os.path.join(HOME, ".vim")
-    vimrc        = os.path.join(HOME, ".vimrc")
+    vimdir = os.path.join(HOME, ".vim")
+    vimrc = os.path.join(HOME, ".vimrc")
     vimrc_target = os.path.join(vimdir, "init.vim")
-    nvimdir      = os.path.join(XDG_CONFIG_HOME, "nvim")
+    nvimdir = os.path.join(XDG_CONFIG_HOME, "nvim")
     deploy_repo(repo, ".vim", options, allow_uninstall='no')
     # link for standard vim
     if options.overwrite:
@@ -312,11 +310,11 @@ def deploy_repo(
 
 
 def mkdir(directory):
-    """ Recursive mkdir
+    """Recursive mkdir
 
-        If directory already exists, do nothing.
-        If directory cannot be created, raise OSError
-        If parent directories do not exist, make them as well
+    If directory already exists, do nothing.
+    If directory cannot be created, raise OSError.
+    If parent directories do not exist, make them as well.
     """
     if os.path.isdir(directory):
         pass
@@ -332,22 +330,23 @@ def mkdir(directory):
 
 
 def get(url, destination, options, make_exec=False):
-    """ Download the file at the given URL to destination (relative to HOME).
-        If make_exec is True, also make it executable.
+    """Download the file at the given URL to destination (relative to HOME).
 
-        If the file already exists, an OSError will be raise, unless
-        options.overwrite is True.
+    If `make_exec` is True, also make it executable.
 
-        If options.uninstall is True, destination will be deleted if it exists.
+    If the file already exists, an `OSError` will be raised, unless
+    `options.overwrite` is True.
+
+    If `options.uninstall` is True, destination will be deleted if it exists.
     """
     destination = os.path.join(HOME, destination)
     mkdir(os.path.split(destination)[0])
     if is_file_or_link(destination):
-        if (options.overwrite or options.uninstall):
-            if (options.uninstall and not options.quiet):
+        if options.overwrite or options.uninstall:
+            if options.uninstall and not options.quiet:
                 print("removing %s" % destination)
             os.unlink(destination)
-            if (options.uninstall):
+            if options.uninstall:
                 return
         else:
             raise OSError("File %s already exists" % destination)
@@ -359,12 +358,12 @@ def get(url, destination, options, make_exec=False):
     if make_exec:
         perms = os.stat(destination)
         # chmod a+x
-        os.chmod(destination, perms.st_mode
-                 |  stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
+        os.chmod(destination, perms.st_mode |
+                 stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
 
 
 def git_update(folder=DOTFILES, quiet=False):
-    """ Perform an update of the repository in the given folder """
+    """Perform an update of the repository in the given folder"""
     git = which('git')
     stdout = None
     if quiet:
@@ -388,10 +387,9 @@ def git_update(folder=DOTFILES, quiet=False):
 
 
 def run_duti(quiet=False, handlers='handlers.duti'):
-    """ Run the duti utility, which sets up handlers for opening files on
-        MacOS
+    """Run the duti utility, which sets up handlers for opening files on MacOS
 
-        The path to the handlers file is relative to DOTFILES
+    The path to the handlers file is relative to DOTFILES
     """
     duti = which('duti')
     if duti is not None:
@@ -407,9 +405,7 @@ def run_duti(quiet=False, handlers='handlers.duti'):
 
 
 def set_crontab(quiet=False, crontab_file='~/.crontab'):
-    """
-    Set the crontab to the given crontab_file
-    """
+    """Set the crontab to the given crontab_file"""
     crontab = which('crontab')
     crontab_file = os.path.expanduser(crontab_file)
     if crontab is not None:
@@ -425,9 +421,8 @@ def set_crontab(quiet=False, crontab_file='~/.crontab'):
             raise OSError("crontab returned nonzero exist status (%s)" % ret)
 
 
-
 def get_options(argv=None):
-    """ Parse command line options. return options object """
+    """Parse command line options. return options object"""
     if argv is None:
         argv = sys.argv
     arg_parser = OptionParser(usage="usage: %prog [options]")
@@ -444,7 +439,7 @@ def get_options(argv=None):
 
 
 def main(deploy, argv=None):
-    """ Main function """
+    """Main function, executing `deploy` routine"""
     options = get_options(argv)
     try:
         git_update(folder=DOTFILES, quiet=options.quiet)
